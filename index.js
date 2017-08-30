ff0bdb5f741029da52ea139348897be080fdc62a
torben <t.sickert[~at~]gmail.com>
2017-08-31 00:59:45 +0200

    Automatic update to new version.

--- index.js
+++ index.js
@@ -27,30 +27,44 @@ try {
     module.require('source-map-support/register')
 } catch (error) {}
 import tinymce from 'tinymce'
-import 'tinymce/themes/modern/theme'
+import 'tinymce/plugins/anchor'
 import 'tinymce/plugins/advlist'
+import 'tinymce/plugins/anchor'
+import 'tinymce/plugins/autoresize'
 import 'tinymce/plugins/autolink'
+import 'tinymce/plugins/charmap'
+import 'tinymce/plugins/code'
+import 'tinymce/plugins/contextmenu'
+import 'tinymce/plugins/fullscreen'
+import 'tinymce/plugins/help'
+import 'tinymce/plugins/hr'
+import 'tinymce/plugins/image'
+import 'tinymce/plugins/imagetools'
+import 'tinymce/plugins/insertdatetime'
 import 'tinymce/plugins/lists'
 import 'tinymce/plugins/link'
-import 'tinymce/plugins/charmap'
+import 'tinymce/plugins/media'
+import 'tinymce/plugins/nonbreaking'
+import 'tinymce/plugins/noneditable'
+import 'tinymce/plugins/pagebreak'
+import 'tinymce/plugins/paste'
+import 'tinymce/plugins/preview'
 import 'tinymce/plugins/print'
 import 'tinymce/plugins/preview'
-import 'tinymce/plugins/anchor'
 import 'tinymce/plugins/searchreplace'
-import 'tinymce/plugins/visualblocks'
-import 'tinymce/plugins/code'
-import 'tinymce/plugins/fullscreen'
-import 'tinymce/plugins/insertdatetime'
-import 'tinymce/plugins/media'
+import 'tinymce/plugins/spellchecker'
 import 'tinymce/plugins/table'
-import 'tinymce/plugins/contextmenu'
-import 'tinymce/plugins/paste'
+import 'tinymce/plugins/textcolor'
+import 'tinymce/plugins/toc'
+import 'tinymce/plugins/visualblocks'
+import 'tinymce/plugins/visualchars'
+import 'tinymce/plugins/wordcount'
+import 'tinymce/themes/modern/theme'
 require.context(
-  'file?name=editorAssets.compiled/[path][name].[ext]&context=node_modules/tinymce!tinymce/skins',
-  true,
-  /.*/
-)
-
+    'file?name=editorAssets.compiled/[path][name].[ext]&' +
+        'context=node_modules/tinymce!tinymce/skins',
+    true,
+    /.*/)
 import './index.css'
 // endregion
 export const $:any = binding
@@ -382,6 +396,8 @@ export default class WebsiteBuilder extends $.Tools.class {
             type = 'advanced'
         else if (attributeName.toLowerCase().includes('simple'))
             type = 'simple'
+        else if (attributeName.toLowerCase().includes('raw'))
+            type = 'raw'
         domNode.addEventListener('click', ():void =>
             tinymce.init(this.constructor.extendObject(
                 true, {}, this._options.inPlaceEditor.default,
